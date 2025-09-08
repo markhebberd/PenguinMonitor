70c9aae29d9b7df5bc9b3b43a1d12d142eb2d989
Mark Hebberd <markhebberd@gmail.com>
2025-09-09 11:56:15 +1200

    V29: fix PG expected dates.

--- Models/BoxPredictedDates.cs
+++ Models/BoxPredictedDates.cs
@@ -14,26 +14,24 @@
             try
             {
                 DateTime estHatch = DateTime.Parse(estHatchDate);
-                if (estHatch.AddDays(3) > DateTime.Now)
+                if (estHatch.AddDays(3) >= DateTime.Today)
                     return "Hatch" + getDateString(estHatch);
 
-                DateTime estPG = DateTime.Parse(estHatchDate);
-                if (estPG.AddDays(3) > DateTime.Now)
+                DateTime estPG = DateTime.Parse(estPGDate);
+                if (estPG.AddDays(3) >= DateTime.Today)
                     return "PG" + getDateString(estPG);
 
                 DateTime chipStart = DateTime.Parse(chipWindowStart);
-                if (chipStart.AddDays(3) > DateTime.Now)
+                if (chipStart.AddDays(3) >= DateTime.Today)
                     return "Chip" + getDateString(chipStart);
 
                 DateTime estFledge = DateTime.Parse(estFledgeDate);
-                if (estFledge.AddDays(3) > DateTime.Now)
-                    return "Fledge" + getDateString(estFledge);
+                return "Fledge" + getDateString(estFledge);
             }
             catch
             {
                 return "BreedingDateError";
             }
-            return "";
         }
 
         private string getDateString(DateTime expectedDate)
@@ -43,19 +41,19 @@
             {
                 return " today";
             }
-            if ((expectedDate.Date - today).TotalDays == 1)
+            if ((expectedDate.Date - today).TotalDays == 1 && expectedDate > today)
             {
                 return " tomorrow";
             }
-            if ((expectedDate.Date - today).TotalDays == -1)
+            if ((today - expectedDate.Date).TotalDays == 1)
             {
                 return " yesterday";
             }
             if (expectedDate > today)
             {
-                return " in " + Math.Ceiling((expectedDate - DateTime.Now).TotalDays) + " days";
+                return " in " + Math.Ceiling((expectedDate - today).TotalDays) + " days";
             }
-            return " " + Math.Ceiling((DateTime.Now - expectedDate).TotalDays) + " days ago";
+            return " " + Math.Ceiling((today - expectedDate).TotalDays) + " days ago";
         }
     }
 }
